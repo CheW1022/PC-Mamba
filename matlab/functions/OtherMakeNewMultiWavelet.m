f971a19a5c0da66abdfb66486514d7e42e1dfb0b
Che Wang <117015770+CheW1022@users.noreply.github.com>
2026-05-03 22:54:46 +0800

    Delete matlab/functions

--- matlab/functions/OtherMakeNewMultiWavelet.m
+++
@@ -1,8 +0,0 @@
-function [ng]=OtherMakeNewMultiWavelet(ng,g1,h1)
-     [N,M]=size(ng);
-     gg=zeros(2*N-1,M);
-     gg(1:2:end,:)=ng;
-     ng1=convn(gg,g1(:));
-     ng2=convn(gg,h1(:));
-     ng=[ng1,ng2];
-end
